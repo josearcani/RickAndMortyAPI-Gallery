8e3939827a5be47ee437bc070bca5ffb3c378de0
joseArc23 <juanjose.arcani2309@gmail.com>
2020-12-07 15:25:49 -0400

    Frontend completed, just a section is not

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const c = console.log
+
+// const URL = 
+
+// const cs ={
+//   characters:"https://rickandmortyapi.com/api/character",
+//   locations:"https://rickandmortyapi.com/api/location",
+//   episodes:"https://rickandmortyapi.com/api/episode"
+// }
+
+window.addEventListener('DOMContentLoaded', function() {
+  c('fully loaded')
+})
